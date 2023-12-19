a0bc0346007f5c4c25aa4c3351234010be1a093d
Jiruooo <125987814+Jiruooo@users.noreply.github.com>
2023-12-19 18:15:10 +0100

    Add files via upload

--- Musubi/script.js
+++ Musubi/script.js
@@ -8,6 +8,8 @@ document.addEventListener("DOMContentLoaded", function () {
     produits.forEach(function (produit) {
         const boutonAjouter = produit.querySelector(".ajouter");
         const boutonRetirer = produit.querySelector(".retirer");
+        const spanQuantite = produit.querySelector(".quantite-commandee"); // Sélection du span pour la quantité
+
         const nomProduit = boutonAjouter.dataset.nom;
         const prixProduit = parseFloat(boutonAjouter.dataset.prix);
 
@@ -23,18 +25,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
             prixTotal += prixProduit;
             afficherPanier();
+            // Mettre à jour la quantité affichée
+            spanQuantite.textContent = panier[nomProduit].quantite;
         });
 
         boutonRetirer.addEventListener("click", function () {
             if (panier.hasOwnProperty(nomProduit) && panier[nomProduit].quantite > 0) {
                 panier[nomProduit].quantite--;
                 prixTotal -= prixProduit;
-                
+
                 if (panier[nomProduit].quantite === 0) {
                     delete panier[nomProduit];
                 }
-                
+
                 afficherPanier();
+                // Mettre à jour la quantité affichée
+                spanQuantite.textContent = panier[nomProduit] ? panier[nomProduit].quantite : 0;
             }
         });
     });
